a010f0ed4ef8de5ea0cbe2f1c1c89e75bd3a6e0a
zubquzaini <zubliquzaini@gmail.com>
2023-06-27 06:38:30 +0800

    Update label components

--- src/components/label/templates.js
+++ src/components/label/templates.js
@@ -3,9 +3,11 @@ import React from "react";
 // import {Text} from 'react-native';
 // import {getStyle} from '../tailwind';
 import LabelDefault from "./labelDefault";
+import LabelLeft from "./labelLeft";
 
 const TemplateList = {
     "": (props) => <LabelDefault {...props} />,
+    "labelLeft": (props) => <LabelLeft {...props} />,
 };
 
 export default TemplateList;
