8a8cc06a0f47fdff212871ea4c6ce779e35fe8f1
zubquzaini <zubliquzaini@gmail.com>
2023-06-27 06:46:40 +0800

    Added components to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,32 +1,46 @@
 import React from 'react'
 
+// New components
+import { Demo } from './mae_web/demo'
+import { Header } from './mae_web/components/Header'
+import { Tab } from './mae_web/components/Tab'
+import { ShowModal } from './mae_web/components/Modal'
+
+// Reuse components
+import { Radio } from './@vb/components/Radio'
+
+// Original components
 import { Button } from './button'
 import { Checkbox } from './checkbox'
 import { Fieldset } from './fieldset'
 // import { Image } from "./image";
 import { Label } from './label'
-import { Radiobox } from './radiobox'
+// import { Radiobox } from './radiobox'
 import { Select } from './select'
 // import { Switch } from "./switch";
 import { Text } from './text'
 import { Hook } from "./Hook"
 import { List } from "./List"
 
-
 const Components = {
+
   // image:(props) => <Image {...props} />,
   button: (props) => <Button {...props} />,
   checkbox: (props) => <Checkbox {...props} />,
   fieldset: (props) => <Fieldset {...props} />,
   label: (props) => <Label {...props} />,
-  radiobox: (props) => <Radiobox {...props} />,
+  radiobox: (props) => <Radio {...props} />,
   select: (props) => <Select {...props} />,
   // switch: (props) => <Switch {...props} />,
   text: (props) => <Text {...props} />,
   hook: (props) => <Hook {...props} />,
   list: (props) => <List {...props} />,
 
-  
+  // mae-web-kit
+  demo: (props) => <Demo {...props} />,
+  header: (props) => <Header {...props} />,
+  tab: (props) => <Tab {...props} />,
+  showModal: (props) => <ShowModal {...props} />
 }
 
 export const renderComponent = (type, propsItems) => {
