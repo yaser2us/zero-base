2320f52284eebff6f9ba70af4a38255f5d81abc0
Quzaini Zubli <zubliquzaini@gmail.com>
2023-07-18 14:17:54 +0800

    restructure box

---
+++ src/components/Box/Templates/DefaultBox/DefaultBox.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { DefaultBlock as Block } from "../../../Block/Templates/DefaultBlock";
+
+import "./DefaultBox.scss";
+
+const DefaultBox = (props) => {
+  // const { title, amount,tooltip } = props
+  const { item } = props;
+  const { header, description, tooltip = "" } = item;
+
+  return (
+    <div
+      style={{
+        padding: "24px 16px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#FFFFFF",
+        boxShadow: "0px 4px 16px rgba(0, 0, 0, 0.08)",
+        borderRadius: "8px",
+        marginTop: "1rem",
+        marginBottom: "2rem",
+      }}
+    >
+      <Block
+        titleClassName="box-topBottom-title"
+        tooltip={tooltip.length > 0 ? tooltip : false}
+        title={header}
+      />
+      <div className="box-topBottom-amount">{description}</div>
+    </div>
+  );
+};
+
+export default DefaultBox;
---
+++ src/components/Box/Templates/DefaultBox/index.js
@@ -0,0 +1,1 @@
+export { default as DefaultBox } from "./DefaultBox";
