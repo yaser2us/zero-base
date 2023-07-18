930a31c90dd4916d02f74f12ac4c5fb226f2edfe
Quzaini Zubli <zubliquzaini@gmail.com>
2023-07-18 09:33:00 +0800

    restructure header

---
+++ src/components/Header/templates.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { DefaultHeader } from "./Templates/DefaultHeader";
+
+const TemplateList = {
+  "": (props) => <DefaultHeader {...props} />,
+};
+
+export default TemplateList;
