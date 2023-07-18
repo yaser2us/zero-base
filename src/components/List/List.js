72fc000f9f03aff7f92e3f9bce24ba3181b68c1b
Quzaini Zubli <zubliquzaini@gmail.com>
2023-07-18 14:24:38 +0800

    restructure list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,57 +1,21 @@
-import React, { useState } from 'react'
-import { Button, Picker, Space } from 'antd-mobile'
-import { DownOutline } from "antd-mobile-icons";
-import Block from '../block'
-import Error from '../error'
-import { getStyle } from '../tailwind'
-import './index.scss'
+import React from "react";
 
-const Item = (props) => {
-    const { titleClassName, contentClassName, title, content } = props
-    return (
-        <div className='container-list'>
-            <div className={`${titleClassName} title-list`}>{title}</div>
-            <div className={`${contentClassName} content-list`}>{content}</div>
-        </div>
-    )
-}
+import TemplateList from "./templates";
 
 const List = (props) => {
-    //Access to main props
-    const [visible, setVisible] = useState(false);
+  //Access to main props
+  //Injected at runtime
+  const { item } = props;
 
-    //Injected at runtime
-    const { item, field, managedCallback } = props
+  //Always check to not render with error ;)
+  if (item === undefined) return null;
 
-    //Always check to not render with error ;)
-    if (item === undefined) return null
+  const { templateName = "" } = item;
 
+  //Access to all props that introduced in element.
+  return (
+    (TemplateList[templateName] && TemplateList[templateName](props)) || null
+  );
+};
 
-    const { value, onChange } = field
-    const {
-        theme = '',
-        label = '',
-        tooltip = '',
-        options,
-        placeholder
-    } = item
-
-    const onSelect = (val, extend) => {
-        onChange({ val })
-    }
-
-    const onClick = () => setVisible(true)
-    const onClose = () => setVisible(false)
-    const onConfirm = (val, extend) => onChange(extend.items[0].label)
-
-    console.log(options, 'listttttt')
-    
-    return (
-        options.map(el => {
-            console.log(el, 'listttttt')
-            return Item(el);
-        })
-    )
-}
-
-export default List
+export default List;
