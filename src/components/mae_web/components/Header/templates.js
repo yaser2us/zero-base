a4fbbacf88a7bae2461e7290070122fff3b1ae13
Quzaini Zubli <zubliquzaini@gmail.com>
2023-07-18 09:36:28 +0800

    remove old header component

--- src/components/mae_web/components/Header/templates.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import HeaderDefault from "./HeaderDefault";
-
-
-const TemplateList = {
-    '': (props) => <HeaderDefault {...props} />,
-};
-
-export default TemplateList;
