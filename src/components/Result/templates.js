ecc6ab4ddb778f9f0263d825e546aed58af704e6
Quzaini Zubli <zubliquzaini@gmail.com>
2023-07-18 14:19:28 +0800

    restrucure result

---
+++ src/components/Result/templates.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { DefaultResult } from "./Templates/DefaultResult";
+
+const TemplateList = {
+  "": (props) => <DefaultResult {...props} />,
+};
+
+export default TemplateList;
